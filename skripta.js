e861c5e56af02b2347d4d21f6eeccf8f7971ca77
milicap2108-beep <milicap2108@gmail.com>
2026-02-12 22:49:04 +0100

    aa

--- skripta.js
+++ skripta.js
@@ -2,16 +2,40 @@
 const menuBtn = document.getElementById('meni-dugme');
 const menuOverlay = document.querySelector('.meni-overlay');
 const closeBtn = document.querySelector('.close-dugme');
+const hamburger = document.querySelector('.hamburger'); 
+const hamburgerZ = document.querySelector('.hamburger-z')
 
-// Otvori meni //
-menuBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    menuOverlay.classList.add('active');
-    document.body.style.overflow = 'hidden';
-});
+// Otvori meni - desktop dugme //
+if (menuBtn) {
+    menuBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        menuOverlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+}
 
-// Zatvori meni //
-closeBtn.addEventListener('click', function() {
-    menuOverlay.classList.remove('active');
-    document.body.style.overflow = 'auto';
-});
+// Otvori meni - hamburger žuti //
+if (hamburger) {
+    hamburger.addEventListener('click', function(e) {
+        e.preventDefault();
+        menuOverlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+}
+
+// Otvori meni - hamburger zeleni //
+if (hamburgerZ) {
+    hamburgerZ.addEventListener('click', function(e) {
+        e.preventDefault();
+        menuOverlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+}
+
+// Zatvori meni - X dugme //
+if (closeBtn) {
+    closeBtn.addEventListener('click', function() {
+        menuOverlay.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    });
+}
